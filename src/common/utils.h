e63de31513e07690a12b73fd83d3659519bea079
Paz <63509564+mpazmarcato@users.noreply.github.com.>
2024-10-21 11:09:58 -0300

    style: modified merge fuction style

--- src/common/utils.h
+++ src/common/utils.h
@@ -23,36 +23,24 @@ void merge(std::vector<int>& arr, int left, int mid, int right) {
     for (int i = 0; i < n1; i++){
         L[i] = arr[left + i];
     }
+
     for (int j = 0; j < n2; j++){
         R[j] = arr[mid + 1 + j];
     }
     
-    int i = 0, j = 0;
-    int k = left;
+    int i = 0, j = 0, k = left;
 
     while (i < n1 && j < n2) {
-      if (L[i] <= R[j]) {
-          arr[k] = L[i];
-          i++;
-      }
-      else {
-          arr[k] = R[j];
-          j++;
-      }
-      k++;
+        if (L[i] <= R[j]) { 
+            arr[k++] = L[i++]; 
+        } else { 
+            arr[k++] = R[j++]; 
+        }
     }
 
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
+    
 }
 
 void swap(int array[], int index1, int index2) { 
