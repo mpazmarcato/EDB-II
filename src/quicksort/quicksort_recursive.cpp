40b6e169304f59bf5f83f4dffd4c86220ad9336c
Vitugome3s <victor.aguiar08@hotmail.com>
2024-10-31 20:51:32 -0300

    Conclusão análise

--- src/quicksort/quicksort_recursive.cpp
+++ src/quicksort/quicksort_recursive.cpp
@@ -5,23 +5,28 @@
 
 using namespace std;
 
-int partition(vector<int>& arr, int low, int high) {
+int partition(vector<int> &arr, int low, int high)
+{
     int middle = low + (high - low) / 2;
     int pivo = arr[middle];
 
     int start = low;
     int end = high;
 
-    while (start <= end) {
-        while (arr[start] < pivo) {
+    while (start <= end)
+    {
+        while (arr[start] < pivo)
+        {
             start++;
         }
 
-        while (arr[end] > pivo) {
+        while (arr[end] > pivo)
+        {
             end--;
         }
 
-        if (start <= end) {
+        if (start <= end)
+        {
             std::swap(arr[start], arr[end]);
             start++;
             end--;
@@ -30,15 +35,18 @@ int partition(vector<int>& arr, int low, int high) {
     return start;
 }
 
-void quickSort(vector<int>& array, int left, int right) {
-    if (left < right) {
+void quickSort(vector<int> &array, int left, int right)
+{
+    if (left < right)
+    {
         int pivot = partition(array, left, right);
-        quickSort(array, left, pivot - 1); 
-        quickSort(array, pivot, right);     
+        quickSort(array, left, pivot - 1);
+        quickSort(array, pivot, right);
     }
 }
 
-int main() {
+int main()
+{
     vector<int> data = {8, 7, 2, 1, 0, 9, 6};
     printVector(data);
 
